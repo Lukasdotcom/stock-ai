4aecc9deddb5dcbbdd534e83e92fb1f7d9d902ba
Lukasdotcom <lukas@lschaefer.xyz>
2022-04-09 14:03:59 -0400

    Added support for caching stock data

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,1 +1,7 @@
+service mariadb start
+mysql -u root -e "CREATE DATABASE IF NOT EXISTS stock"
+mysql -u root -e "CREATE user IF NOT EXISTS 'stock-ai'@'localhost' IDENTIFIED BY 'password';"
+mysql -u root -e "GRANT ALL PRIVILEGES ON *.* TO 'stock-ai'@localhost IDENTIFIED BY 'password';"
+mysql -u root --database=stock -e "CREATE TABLE IF NOT EXISTS stocks (ticker varchar(10), time int, close float);"
+mysql -u root --database=stock -e "CREATE TABLE IF NOT EXISTS stockMeta (ticker varchar(10), lastUpdate int);"
 npm run $TYPE
--- pages/stocks/[name].js
+++ pages/stocks/[name].js
@@ -23,17 +23,69 @@ export default function Graph( { title, stock } ) {
   }
 
 export async function getServerSideProps(context, res) {
+    const day = Math.floor(Date.now() / 1000 / 3600 / 24)
     const title = context.params.name
-    const response = await fetch(`https://query1.finance.yahoo.com/v7/finance/download/${title}?period1=0&period2=2229456811&interval=1d&events=history&includeAdjustedClose=true`)
-    // Reads the csv file
+    var mysql = require('mysql');
+    var connection = mysql.createConnection({
+        host     : '127.0.0.1',
+        user     : 'stock-ai',
+        password : "password",
+        database : 'stock'
+        });
+    const cached = await new Promise((resolve) => connection.query("SELECT * FROM stockMeta WHERE ticker=?", [title], function (error, results, fields) {
+        resolve(results)
+    })).then((val => {
+        if (val && val.length > 0) {
+            return parseInt(val[0].lastUpdate) == day
+        } else {
+            return false
+        }
+    }))
     var stock = []
-    const parser = parse(await response.text(), {
-        columns: true,
-        skip_empty_lines: true
-    })
-    for await (const record of parser) {
-        stock.push(parseFloat(record["Close"]))
+    if (! cached) {
+        // Gets the latest data from yahoo finance
+        const response = await fetch(`https://query1.finance.yahoo.com/v7/finance/download/${title}?period1=0&period2=2229456811&interval=1d&events=history&includeAdjustedClose=true`)
+        // Reads the csv file
+        const parser = parse(await response.text(), {
+            columns: true,
+            skip_empty_lines: true
+        })
+        for await (const record of parser) {
+            stock.push(parseFloat(record["Close"]))
+        }
+        // Checks that the stock exists
+        if (stock.length > 0) {
+            var connection2 = mysql.createConnection({
+                host     : '127.0.0.1',
+                user     : 'stock-ai',
+                password : "password",
+                database : 'stock'
+                });
+            // Adds the data to the database
+            let count = 0
+            connection2.query('DELETE FROM stocks WHERE ticker=?;', [title]);
+            stock.forEach(element => {
+                count ++;
+                connection2.query('INSERT INTO stocks VALUES (?, ?, ?)', [title, count, element]);
+            })
+            connection2.query("DELETE FROM stockMeta WHERE ticker=?;",[title])
+            connection2.query("INSERT INTO stockMeta VALUES(?, ?);", [title, day])
+            connection2.end()
+        }
+    } else {
+        stock = await new Promise((resolve) => {
+            connection.query("SELECT * FROM stocks WHERE ticker=? ORDER BY time ASC", [title], function (error, results2, fields) {
+                let stockData = []
+                let record = results2 ? results2 : []
+                record.forEach(element => {
+                    stockData.push(parseFloat(element.close))
+                })
+                console.log(stockData)
+                resolve(stockData)
+            })
+        }).then((val => {return val}))
     }
+    connection.end();
     if (stock.length < 1) {
         return {
             notFound: true,
@@ -41,7 +93,7 @@ export async function getServerSideProps(context, res) {
     }
     return {
         props : {
-            stock, title,
+            stock, title, 
         },
     }
 }
