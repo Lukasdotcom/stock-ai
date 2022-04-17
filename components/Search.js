826c67bab305608909500bd88b082df71633a3cf
Lukasdotcom <lukas@lschaefer.xyz>
2022-04-16 20:10:47 -0400

    Improves the search bar by banning bad characters

--- components/Search.js
+++ components/Search.js
@@ -4,19 +4,24 @@ import { useState } from "react";
 
 const Layout = () => {
     async function search(val) {
+        // Makes sure that only a small list of characters are allowed to make bugs less rare
+        val = val.replace(/[^a-z0-9\^]/gi, "")
         var newSearchResult = []
-        if (val) {
+        const same = val == searchTerm
+        setNewSearchTerm(val)
+        if (val && val != "" && ! same) {
             const result = await fetch(`/api/search/${val}`)
             newSearchResult = await result.json()
         }
         setSearchResult(newSearchResult)
     }
     const [searchResult, setSearchResult] = useState([]);
+    const [searchTerm, setNewSearchTerm] = useState("DUMMY");
     return (
     <>
         <wrapper className={searchStyles.wrapper}>
         <div className={searchStyles.searchInput}>
-            <input type={"text"} placeholder={"Type Here to Search..."} onClick={(val) => {search(val.target.value)}} onChange={(val) => {search(val.target.value)}}></input>
+            <input type={"text"} value={searchTerm} placeholder={"Type Here to Search..."} onClick={(val) => {search(val.target.value)}} onChange={(val) => {search(val.target.value)}}></input>
             {searchResult.length > 0 ? <ul className={searchStyles.autocom}>
                 {searchResult.map((result) => (<li onClick={() => {setSearchResult([])}} key={result.symbol}><Link href={`/stocks/${result.symbol}`}>{`${result.shortname} - ${result.symbol}`}</Link></li>))}
             </ul> : ""}
--- pages/api/search/[term].js
+++ pages/api/search/[term].js
@@ -1,4 +1,6 @@
 export default async function handler({ query: { term } }, res) {
+    // Makes sure that only a small list of characters are allowed
+    term = term.replace(/[^a-z0-9\^]/gi, "")
     const result = await fetch(`https://query2.finance.yahoo.com/v1/finance/search?q=${term}&lang=en-US&region=US&quotesCount=3&enableFuzzyQuery=false&quotesQueryId=tss_match_phrase_query&multiQuoteQueryId=multi_quote_single_token_query`)
     const search = await result.json()
     res.status(200).json(search.quotes)
