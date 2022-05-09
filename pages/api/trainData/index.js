df1f1482bf072dc220a770b4aa74a7ca665587ae
Lukasdotcom <lukas@lschaefer.xyz>
2022-05-09 18:09:32 -0400

    fixes bug with trainData api

--- pages/api/trainData/index.js
+++ pages/api/trainData/index.js
@@ -6,7 +6,7 @@ export default async function handler(req, res) {
     var mysql = require('mysql')
     // Makes sure the admin is logged in
     if (session) {
-        var connection = mysql.createConnection({
+        var connection = await mysql.createConnection({
             host     : mysql_host,
             user     : mysql_user,
             password : mysql_password,
@@ -22,22 +22,19 @@ export default async function handler(req, res) {
                 })
                 break;
             case "PUT": // Will update the training data for the bot
-                await new Promise((resolve) => {
+                await new Promise((resolve, reject) => {
                     // Will make sure that the stock exists
                     connection.query("SELECT * FROM stockMeta WHERE ticker=?", [req.body.new.ticker], function(error, results2, fields) {
-                        resolve(results2)
+                        // Will make sure that the values are valid
+                        if ((results2.length > 0 && req.body.new.start > 0 ) && (req.body.new.end > 0 && req.body.new.end > req.body.new.start )) {
+                            // Will delete the old data
+                            connection.query("DELETE FROM taskStocks WHERE (ticker=? and name=?) and (start=? and end=?)", [req.body.old.ticker, bot, req.body.old.start, req.body.old.end], function(error, results3, fields) {
+                                resolve("Changed data")
+                            })
+                        } else {
+                            reject("Invalid Values")
+                        }
                     })
-                }).then((val) => {
-                    // Will make sure that the values are valid
-                    if ((val.length > 0 && req.body.new.start > 0 ) && (req.body.new.end > 0 && req.body.new.end > req.body.new.start )) {
-                        // Will delete the old data
-                        connection.query("DELETE FROM taskStocks WHERE (ticker=? and name=?) and (start=? and end=?)", [req.body.old.ticker, bot, req.body.old.start, req.body.old.end], function(error, results3, fields) {
-                            resolve("Changed data")
-                        })
-                    } else {
-                        console.log("ERROR")
-                        throw "Invalid Values"
-                    }
                 }).then((val) => {
                     // Will add the new data
                     connection.query("INSERT INTO taskStocks VALUES (?, ?, ?, ?)", [bot, req.body.new.ticker, req.body.new.start, req.body.new.end])
